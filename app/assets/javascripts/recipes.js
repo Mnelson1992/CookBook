1e1303c71e9158aec88c673343d15f2a58f0a5b1
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-05-08 10:50:48 -0400

    cleaned up recipes.js

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -3,12 +3,10 @@ $(document).ready(function(){
 
   $('#ingredients').on('click', function(){
     const id = $(this).data('id')
-    //console.log(id)
     $.get(`http://localhost:3000/recipes/${id}/ingredients.json`, function(data){
       data.forEach(function(ingredient){
         $('#ingredients_list').append(`<li>${ingredient.name}</li>`)
       })
-
     })
   })
 
