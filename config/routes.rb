92ca33e59684ecdb02e93f85bd739e6afaeaea58
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-05-09 15:05:40 -0400

    Added comment div to recipe show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
     resources :ingredients
   end
 
-  
+  resources :recipes, only: [:index, :show] do
+    resources :comments, only: [:index, :create]
+  end
+
+
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
