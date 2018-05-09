0c6bc16e6e754707da1333c07f3580f93ec7c837
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-05-09 15:12:00 -0400

    Added the comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,7 @@
+class CommentsController < ApplicationController
+ def index
+   @recipe = Recipe.find(params[:recipe_id])
+   @comments = @recipe.comments.order(created_at: :asc)
+   render json: @comments, status: 200
+ end
+end
