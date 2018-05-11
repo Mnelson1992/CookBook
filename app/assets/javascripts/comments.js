ebeef4f7bfecde367f0c1e859890a7c458f5f573
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-05-11 14:47:57 -0400

    Changed the wording of the form submit button

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,15 +1,12 @@
+
 $(document).ready(function(){
 
   $('#showComments').on('click', function(){
     const id = $(this).data('id')
     $.get(`http://localhost:3000/recipes/${id}/comments.json`, function(data){
       data.forEach(function(comment){
-        $('#comments_list').append(`<li>${comment.body}</li>`)
+        $('#comments_list').append(`<li><em>"${comment.body}"</em> -- </li>`)
       })
     })
   })
-
-  
-
-
 })
